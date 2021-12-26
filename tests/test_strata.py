b2c5ba5d94650a8b047f084468343c92ee0a69a5
giocaizzi <giocaizzi@gmail.com>
2021-12-26 09:54:29 +0100

    new developement branch to limit CI builds

--- tests/test_strata.py
+++ tests/test_strata.py
@@ -3,11 +3,12 @@ from matplotlib.colors import ListedColormap
 
 
 from matplotlib import colors
-from mplStrater.strata import Symbology
+from mplStrater.strata import Symbology,Legend
 
 class TestSymbology(unittest.TestCase):
 
     def test_color(self):
+        #test symbology structure
         colors=["green","white","blue"]
         s=Symbology(d={},colors=colors)
         self.assertIsInstance(s.d,dict)
@@ -15,10 +16,20 @@ class TestSymbology(unittest.TestCase):
         self.assertIsInstance(s.cmap,ListedColormap)
     
     def test_hatches(self):
+        #test symbology structure
         hatches=["","xxxxxxxxx",""]
         s=Symbology(d={},hatches=hatches)
         self.assertIsInstance(s.d,dict)
-        self.assertEqual(set(s.hatches),set(hatches))    
+        self.assertEqual(set(s.hatches),set(hatches))
+
+class TestLegend(unittest.TestCase):
+
+    def test_init(self):
+        #matrix and hatches are symbology 
+        l=Legend()
+        self.assertIsInstance(l.matrix,Symbology)
+        self.assertIsInstance(l.hatches,Symbology)
+
 
 if __name__=="__main__":
     unittest.main()
