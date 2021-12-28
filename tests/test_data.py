64c1f5a2fce50b11683a694ad109e2f4aaf736ad
giocaizzi <giocaizzi@gmail.com>
2021-12-28 23:01:29 +0100

    test data better

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 from mplStrater.data import StrataFrame
 import geopandas as gp
+import pytest
 
 #first three rows of repository example
 example={'x': {0: 204026.93, 1: 204026.93, 2: 204026.93},
@@ -35,14 +36,35 @@ class TestStrataFrame(unittest.TestCase):
         d={}
         self.assertRaises(ValueError,StrataFrame,d,epsg)
     
-    def test_init_correct(self):
-        sf=StrataFrame(self.df,epsg)
-        self.assertIsInstance(sf.strataframe,gp.GeoDataFrame)
-    
-    # @patch(StrataFrame,"__init__",partial_init)
-    # def test_set_df(self):
+    # def test_init_correct(self):
     #     sf=StrataFrame(self.df,epsg)
-    #     pass
+    #     self.assertIsInstance(sf.strataframe,gp.GeoDataFrame)
+
+    def test_group_layers(self):
+        profondita=StrataFrame.group_layers(self.df).reset_index()
+        profondita.set_index("punto",inplace=True)
+        profondita_l=profondita.at["SI01","layers"].tolist()
+        original_len=len(self.df[self.df["punto"]=="SI01"])
+        #check that has one more value than the original
+        self.assertEqual(len(profondita_l),original_len+1)
+        #check that is sorted
+        self.assertListEqual(profondita_l,sorted(profondita_l))
+        #check that there are no duplicates
+        self.assertEqual(len(profondita_l), len(set(profondita_l)))
+
+    @pytest.mark.wip
+    def test_group_data(self):
+        data=StrataFrame.group_data(self.df,"matrice","lista_matrici").reset_index()
+        data.set_index("punto",inplace=True)
+        data_list=data.at["SI01","lista_matrici"]
+        original_len=len(self.df[self.df["punto"]=="SI01"])
+        #check that has one more value than the original
+        self.assertEqual(len(data_list),original_len+1)
+    
+    # @patch.object(StrataFrame,"__init__",partial_init)
+    # def test_patched_init(self):
+    #     sf=StrataFrame(self.df,epsg)
+    #     self.assertRaises(AttributeError,sf.strataframe)
 
 if __name__=="__main__":
     unittest.main()
