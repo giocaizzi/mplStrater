749fa8c26ccd50458e6276498a945a7210b6838b
giocaizzi <giocaizzi@gmail.com>
2021-12-29 16:38:07 +0100

    test data

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,16 @@
+from pathlib import Path
+import pytest
+from pandas import read_csv
+
+
+CWD = Path(__file__).resolve()
+FIN = CWD.parent / "test.csv"
+CRS=32633
+
 def pytest_configure(config):
     config.addinivalue_line("markers", "wip: WORK IN PROGRESS")
+
+@pytest.fixture(scope="class")
+def test_data(request):
+    request.cls.test_data=read_csv(FIN)
+    request.cls.epsg=CRS
--- tests/test_data.py
+++ tests/test_data.py
@@ -2,70 +2,67 @@ import unittest
 from unittest.mock import patch
 import numpy as np
 import pandas as pd
+from pandas.core.frame import DataFrame
 from mplStrater.data import StrataFrame
 import geopandas as gp
 import pytest
-import pyproj 
-
-#first three rows of repository example
-example_dataset="tests/test.csv"
-epsg=32633
+import pyproj
 
 def partial_init(self,df,epsg):
     self.epsg=epsg
     self.df=df
 
+@pytest.mark.usefixtures("test_data")
 class TestStrataFrame(unittest.TestCase):
-
-    def setUp(self):
-        self.df=pd.read_csv(example_dataset)
 
     def test_init_fail(self):
         d={}
-        self.assertRaises(ValueError,StrataFrame,d,epsg)
+        self.assertRaises(ValueError,StrataFrame,d,self.epsg)
+
+    def test_fixture_test_data(self):
+        assert hasattr(self, "test_data")
+        assert isinstance(self.test_data, pd.DataFrame)
 
     def test_group_layers(self):
-        profondita=StrataFrame.group_layers(self.df).reset_index()
+        profondita=StrataFrame.group_layers(self.test_data).reset_index()
         profondita.set_index("punto",inplace=True)
         profondita_l=profondita.at["SI01","layers"].tolist()
-        original_len=len(self.df[self.df["punto"]=="SI01"])
+        original_len=len(self.test_data[self.test_data["punto"]=="SI01"])
         #check that has one more value than the original
-        self.assertEqual(len(profondita_l),original_len+1)
+        assert len(profondita_l)== original_len+1
         #check that is sorted
-        self.assertListEqual(profondita_l,sorted(profondita_l))
+        assert profondita_l==sorted(profondita_l)
         #check that there are no duplicates
-        self.assertEqual(len(profondita_l), len(set(profondita_l)))
+        assert  len(profondita_l) == len(set(profondita_l))
 
     def test_group_data(self):
-        data=StrataFrame.group_data(self.df,"matrice","lista_matrici").reset_index()
+        data=StrataFrame.group_data(self.test_data,"matrice","lista_matrici").reset_index()
         data.set_index("punto",inplace=True)
         data_list=data.at["SI01","lista_matrici"]
-        original_len=len(self.df[self.df["punto"]=="SI01"])
+        original_len=len(self.test_data[self.test_data["punto"]=="SI01"])
         #check that has one more value than the original
         self.assertEqual(len(data_list),original_len+1)
 
     @patch.object(StrataFrame,"__init__",partial_init)
     def test_patched_init(self):
-        sf=StrataFrame(self.df,epsg)
+        sf=StrataFrame(self.test_data,self.epsg)
         #lambda function otherwise throws attribute error
         self.assertRaises(AttributeError,lambda: sf.strataframe)
 
-    @pytest.mark.wip
     @patch.object(StrataFrame,"__init__",partial_init)
     def test_set_df(self):
-        sf=StrataFrame(self.df,epsg)
+        sf=StrataFrame(self.test_data,self.epsg)
         sf.set_df()
         #test is df
         self.assertIsInstance(sf.strataframe,pd.DataFrame)
-        #test points are the same
-        punti_in=self.df.punto.unique().tolist()
+        #test points are the sameself.
+        punti_in=self.test_data.punto.unique().tolist()
         punti_out=sf.strataframe.punto.unique().tolist()
         self.assertEqual(len(punti_in),len(punti_out))
         
-    @pytest.mark.wip
     @patch.object(StrataFrame,"__init__",partial_init)
     def test_geo_df(self):
-        sf=StrataFrame(self.df,epsg)
+        sf=StrataFrame(self.test_data,self.epsg)
         sf.set_df()
         sf.geodf()
         #crs is a pyproj object
